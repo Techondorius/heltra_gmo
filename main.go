abc1033c07b3798291767e6d909679abe8350b12
Kyosuke Fujita <kurofuku01@gmail.com>
2022-08-10 20:17:18 +0900

    main.go整形

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"heltra_gmo/middleware"
 	"heltra_gmo/pkg/controller"
 	"heltra_gmo/pkg/model"
-	"log"
 	"net/http"
 	"time"
 )
@@ -25,16 +24,12 @@ func main() {
 	}
 
 	r := gin.Default()
-
 	r.Use(middleware.Logger())
 
 	// Cors
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
 	r.Use(cors.New(config))
-
-	// ルーティング
-	//routing.Routing(r)
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -45,16 +40,7 @@ func main() {
 	r.GET("/getUser", controller.GetUser)
 
 	var GinJWT = middleware.AuthMiddleware
-
 	r.Use(GinJWT.MiddlewareFunc())
-
-	// When you use jwt.New(), the function is already automatically called for checking,
-	// which means you don't need to call it again.
-	errInit := GinJWT.MiddlewareInit()
-
-	if errInit != nil {
-		log.Fatal("authMiddleware.MiddlewareInit() Error:" + errInit.Error())
-	}
 
 	_ = r.Run()
 }
